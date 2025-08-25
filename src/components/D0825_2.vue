fea980bac65bb969e4b48b22f342f52278bbebb3
gdbin1 <ot5037@naver.com>
2025-08-25 15:11:21 +0900

    0825_15:11

--- src/components/D0825_2.vue
+++ src/components/D0825_2.vue
@@ -1,11 +1,21 @@
 <template>
     <div>
-        이프
+        <h1>if문</h1>
+        <h2 style="color: red;">if문</h2>
+        <p v-if="stock">v-if =""true/false 값</p>
+        <p v-else>false값</p>
+        <hr>
+        <p v-if="count > 0">v-if = ""숫자비교로 true/false값</p>
+        <p v-else>false</p>
+
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const stock = ref(true)
+const count = ref(3)
 </script>
 
 <style scoped>
