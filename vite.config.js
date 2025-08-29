37259129a94954d3ba00f792be1153a3dacb70c2
gdbin1 <ot5037@naver.com>
2025-08-29 16:23:43 +0900

    0829dasda

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,36 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  // base 경로를 배포 환경에 맞게 고정합니다.
-  base: '/vue-site/', 
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
-
-
-
-
-
-
-
-
-
+// import { fileURLToPath, URL } from 'node:url'
 // import { defineConfig } from 'vite'
 // import vue from '@vitejs/plugin-vue'
 
-// // https://vite.dev/config/
-// export default defineConfig(({ mode }) => {
-//   return {
-//     plugins: [vue()],
-//     // base: mode === 'production'  ?  '/vue-site/'  :  '/',        
-//      //예: /my-react-app/  
+// export default defineConfig({
+//   // base 경로를 배포 환경에 맞게 고정합니다.
+//   base: '/vue-site/', 
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
 //   }
 // })
+
+
+
+
+
+
+
+
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    // base: mode === 'production'  ?  '/vue-site/'  :  '/',        
+     //예: /my-react-app/  
+  }
+})
